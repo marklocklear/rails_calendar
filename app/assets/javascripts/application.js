2f26e950bd6bfe43d977547bee72c15698073c8d
Mark Locklear <marklocklear@gmail.com>
2013-11-07 16:26:28 -0500

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+//= require jquery
+//= require jquery_ujs
+//= require_self
+//= require_tree .
+
+$.ajaxSetup({
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader("Accept", "text/javascript");
+  }
+});
+
